95e237fe380f7b5b7da7501840904fbf4264fc82
gssab3 <g.sabetta3@studenti.unisa.it>
2024-05-16 23:57:22 +0200

    Varie modifiche d'inizio per il DB
    - DriverManagerConnectionPool.java
    - rimozione della test class
    - inserimento del package it.unisa
    - inserimento dell'sql usato per il nostro db (non ancora menzionato nel
    codice)

---
+++ src/main/java/it/unisa/DriverManagerConnectionPool.java
@@ -0,0 +1,62 @@
+package it.unisa;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class DriverManagerConnectionPool  {
+
+	private List<Connection> freeDbConnections;
+
+	static {	
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver"); //solo per compatibilità delle vers precedenti
+		} catch (ClassNotFoundException e) {
+			System.out.println("DB driver not found:"+ e.getMessage());
+		} 
+	}
+	
+	public DriverManagerConnectionPool() {
+		freeDbConnections = new LinkedList<Connection>();
+	}
+	
+	private synchronized Connection createDBConnection() throws SQLException {
+		Connection newConnection = null;
+		String ip = "localhost";
+		String port = "3306";
+		String db = "RetroCratesDB";
+		String username = "retro";
+		String password = "911MBLSC!a";
+
+		newConnection = DriverManager.getConnection("jdbc:mysql://"+ ip+":"+ port+"/"+db+"?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", username, password);
+		return newConnection;
+	}
+
+
+	public synchronized Connection getConnection() throws SQLException {
+		Connection connection;
+
+		if (!freeDbConnections.isEmpty()) {
+			connection = (Connection) freeDbConnections.get(0);
+			freeDbConnections.remove(0);
+
+			try {
+				if (connection.isClosed())
+					connection = getConnection();
+			} catch (SQLException e) {
+				connection.close();
+				connection = getConnection();
+			}
+		} else {
+			connection = createDBConnection();		
+		}
+
+		return connection;
+	}
+
+	public synchronized void releaseConnection(Connection connection) throws SQLException {
+		if(connection != null) freeDbConnections.add(connection);
+	}
+}
