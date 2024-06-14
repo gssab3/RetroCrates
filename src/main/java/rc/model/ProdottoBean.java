68163c453e6d165f84ca223e706f94cf2a843169
gssab3 <g.sabetta3@studenti.unisa.it>
2024-06-14 19:35:46 +0200

    Modifica completa dell'architettura prodotti

--- src/main/java/rc/model/ProdottoBean.java
+++ src/main/java/rc/model/ProdottoBean.java
@@ -7,6 +7,43 @@ import com.mysql.cj.jdbc.Blob;
 public class ProdottoBean implements Serializable{
 	
 	protected String Produttore; //Enum, vedi la descrizione dei prodotti figli per capire cosa è lecito
+	/*
+	 * Console: Sony, Microsoft, Nintendo, Atari, Sega, Altri
+	 * Videogioco: Ubisoft, Rockstar_Games, Nintendo, Activision, Electronic_Arts, Sega, Naughty_Dog, Microsoft_Studios
+	 * 			   Bethesda, Gearbox_Software, Epic_Games, Capcom, Bandai_Namco_Entertainment, Konami
+	 * Collezionabile: Funko, Nintendo, Pokèmon, BandaiNamco, YouTooz, Sega, Hasbro, Konami
+	 */
+	
+	protected String Genere;
+	/*
+	 * Solo per Videogiochi: Action_Adventure, Picchiaduro, RPG, Sparatutto, Simulazione, Sport, Strategia
+	 */
+	
+	protected String Piattaforma;
+	/*
+	 * Solo per videogiochi: PS1, PS2, PS3, PS4, PS5, Xbox, Xbox_360, Xbox_One, Xbox_Series_X_S, Nintendo_64, GameCube, Wii, Wii_U
+	 * Switch, Game_Boy, Nintendo_DS, Nintendo_3DS, Sega_Mega_Drive, Sega_Master_System, DreamCast, SegaSaturn
+	 * Atari_2600, Atari_5200, Atari_7800, Altro
+	 */
+	 
+	private String TipoGioco;
+	//Solo per Videogioco: "Fisico" , "Digitale"
+	
+	private String TipoProdotto;
+	//Console, Collezionabile, Videogioco
+	
+	private String Categoria;
+	//Solo per collezionabile: Poster, Gadget, Figure, Plush, Audio
+	
+	private String Edizione;
+	/*
+	 * Videogiochi: Standard_Edition, Bronze_Edition, Silver_Edition, Gold_Edition, Platinum_Edition, 
+	 * Diamond_Edition
+	 * G_O_T_Y_Edition, Enhanced_Edition, Ultimate_Edition
+	 * 
+	 * Collezionabile: Normale, Esclusiva, RetroCrates
+	 */
+	
 	protected String IdProdotto; //Esattamente 7 caratteri
 	protected String nome; //Massimo 30 caratteri
 	protected int qta; //>=0
@@ -14,6 +51,8 @@ public class ProdottoBean implements Serializable{
 	protected String descr;
 	protected float costo; // decimal (10,2) >= 0
 	protected Blob picture;
+	protected int stelleTot; //minimo 1 massimo 5
+	protected int recTot; //numero totale di stelle e recensioni;
 	
 	public ProdottoBean()
 	{
@@ -24,6 +63,79 @@ public class ProdottoBean implements Serializable{
 		descr = null;
 		costo = -1;
 		picture = null;
+		stelleTot = 0;
+		recTot = 0;
+		TipoGioco = null;
+		TipoProdotto = null;
+		Categoria = null;
+		Edizione = null;
+		Produttore = null;
+		Genere = null;
+		Piattaforma = null;
+	}
+
+	public String getGenere() {
+		return Genere;
+	}
+
+	public void setGenere(String genere) {
+		Genere = genere;
+	}
+
+	public String getPiattaforma() {
+		return Piattaforma;
+	}
+
+	public void setPiattaforma(String piattaforma) {
+		Piattaforma = piattaforma;
+	}
+
+	public String getTipoGioco() {
+		return TipoGioco;
+	}
+
+	public void setTipoGioco(String tipoGioco) {
+		TipoGioco = tipoGioco;
+	}
+
+	public String getTipoProdotto() {
+		return TipoProdotto;
+	}
+
+	public void setTipoProdotto(String tipoProdotto) {
+		TipoProdotto = tipoProdotto;
+	}
+
+	public String getCategoria() {
+		return Categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		Categoria = categoria;
+	}
+
+	public String getEdizione() {
+		return Edizione;
+	}
+
+	public void setEdizione(String edizione) {
+		Edizione = edizione;
+	}
+
+	public int getStelleTot() {
+		return stelleTot;
+	}
+
+	public void setStelleTot(int stelleTot) {
+		this.stelleTot = stelleTot;
+	}
+
+	public int getRecTot() {
+		return recTot;
+	}
+
+	public void setRecTot(int recTot) {
+		this.recTot = recTot;
 	}
 
 	public String getProduttore() {
---
+++ src/main/java/rc/model/ProdottoDAODataSource.java
@@ -0,0 +1,253 @@
+package rc.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.LinkedList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.awt.image.BufferedImage;
+
+
+public class ProdottoDAODataSource implements IBeanDAO<ProdottoBean> {
+
+	private static DataSource ds;
+
+	static {
+		try {
+			Context initCtx = new InitialContext();
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+
+			ds = (DataSource) envCtx.lookup("jdbc/retrocrates");
+
+		} catch (NamingException e) {
+			System.out.println("Error:" + e.getMessage());
+		}
+	}
+
+	private static final String TABLE_NAME = "Prodotto";
+
+	@Override
+	public synchronized void doSave(ProdottoBean prodotto) throws SQLException {
+		
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		int err = 0;
+		BufferedImage image = null;
+		String insertSQL = "INSERT INTO " + TABLE_NAME
+				+ " (IdProdotto, Nome, Descrizione, Qta, Disponibile, Foto, Costo, stelleTot, recTot, Produttore, Genere, Piattaforma, TipoGioco, TipoProdotto, Categoria, Edizione) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(insertSQL);
+			preparedStatement.setString(1, prodotto.getIdProdotto());
+			preparedStatement.setString(2, prodotto.getNome());
+			preparedStatement.setString(3, prodotto.getDescr());
+			preparedStatement.setInt(4, prodotto.getQta());
+			preparedStatement.setBoolean(5, prodotto.isDisp());
+			preparedStatement.setBlob(6, prodotto.getPicture());
+			preparedStatement.setFloat(7, prodotto.getCosto());
+			preparedStatement.setInt(8, prodotto.getStelleTot());
+			preparedStatement.setInt(9, prodotto.getRecTot());
+			preparedStatement.setString(10, prodotto.getProduttore());
+			preparedStatement.setString(11, prodotto.getGenere());
+			preparedStatement.setString(12, prodotto.getPiattaforma());
+			preparedStatement.setString(13,  prodotto.getTipoGioco());
+			preparedStatement.setString(14, prodotto.getTipoProdotto());
+			preparedStatement.setString(15,  prodotto.getCategoria());
+			preparedStatement.setString(16, prodotto.getEdizione());
+			preparedStatement.executeUpdate();
+			connection.commit();
+
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+	}
+
+	@Override
+	public synchronized boolean doDelete(String code) throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		int result = 0;
+
+		String deleteSQL = "DELETE FROM " + TABLE_NAME + " WHERE IdProdotto = ?";
+
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(deleteSQL);
+			preparedStatement.setString(1, code);
+			result = preparedStatement.executeUpdate();
+			connection.commit();
+
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+		return (result != 0);
+	}
+
+	@Override
+	public synchronized Collection<ProdottoBean> doRetrieveAll(String order) throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		Collection<ProdottoBean> prodotti = new LinkedList<ProdottoBean>();
+
+		String selectSQL = "SELECT * FROM " + TABLE_NAME;
+
+		if (order != null && !order.equals("")) {
+			selectSQL += " ORDER BY " + order;
+		}
+
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(selectSQL);
+
+			ResultSet rs = preparedStatement.executeQuery();
+
+			while (rs.next()) {
+				int err=0;
+				ProdottoBean bean = new ProdottoBean();
+				
+				bean.setIdProdotto(rs.getString("IdProdotto"));
+				bean.setNome(rs.getString("Nome"));
+				bean.setDescr(rs.getString("Descrizione"));
+				bean.setQta(rs.getInt("Qta"));
+				bean.setDisp(rs.getBoolean("Disponibile"));
+				bean.setPicture((com.mysql.cj.jdbc.Blob) rs.getBlob("Foto"));
+				bean.setCosto(rs.getFloat("Costo"));
+				bean.setStelleTot(rs.getInt("stelleTot"));
+				bean.setRecTot(rs.getInt("recTot"));
+				bean.setProduttore(rs.getString("Produttore"));
+				bean.setGenere(rs.getString("Genere"));
+				bean.setPiattaforma(rs.getString("Piattaforma"));
+				bean.setTipoGioco(rs.getString("TipoGioco"));
+				bean.setTipoProdotto(rs.getString("TipoProdotto"));
+				bean.setCategoria(rs.getString("Categoria"));
+				bean.setEdizione(rs.getString("Edizione"));
+				
+				if(bean.isDisp() == false)
+					err = 1;
+				
+				if(err == 0)
+					prodotti.add(bean);
+			}
+
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+		return prodotti;
+	}
+	
+	@Override
+	public synchronized ProdottoBean doRetrieveByKey(String code) throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ProdottoBean bean = new ProdottoBean();
+		String selectSQL = "SELECT * FROM " + TABLE_NAME + " WHERE IdProdotto = ?";
+		try {
+			connection = ds.getConnection();	
+			preparedStatement = connection.prepareStatement(selectSQL);
+			preparedStatement.setString(1, code);
+			ResultSet rs = preparedStatement.executeQuery();
+			while (rs.next()) {
+				bean.setIdProdotto(rs.getString("IdProdotto"));
+				bean.setNome(rs.getString("Nome"));
+				bean.setDescr(rs.getString("Descrizione"));
+				bean.setQta(rs.getInt("Qta"));
+				bean.setDisp(rs.getBoolean("Disponibile"));
+				bean.setPicture((com.mysql.cj.jdbc.Blob) rs.getBlob("Foto"));
+				bean.setCosto(rs.getFloat("Costo"));
+				bean.setStelleTot(rs.getInt("stelleTot"));
+				bean.setRecTot(rs.getInt("recTot"));
+				bean.setProduttore(rs.getString("Produttore"));
+				bean.setGenere(rs.getString("Genere"));
+				bean.setPiattaforma(rs.getString("Piattaforma"));
+				bean.setTipoGioco(rs.getString("TipoGioco"));
+				bean.setTipoProdotto(rs.getString("TipoProdotto"));
+				bean.setCategoria(rs.getString("Categoria"));
+				bean.setEdizione(rs.getString("Edizione"));
+			}
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+		return bean;
+	}
+	
+	public synchronized Collection<ProdottoBean> doRetrieveByCategory(String category) throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		Collection<ProdottoBean> prodotti = new LinkedList<ProdottoBean>();
+		String selectSQL = "SELECT * FROM " + TABLE_NAME + " WHERE Categoria = ?";
+		try {
+			connection = ds.getConnection();	
+			preparedStatement = connection.prepareStatement(selectSQL);
+			preparedStatement.setString(1, category);
+			ResultSet rs = preparedStatement.executeQuery();
+			while (rs.next()) {
+				int err = 0;
+				ProdottoBean bean = new ProdottoBean();
+				bean.setIdProdotto(rs.getString("IdProdotto"));
+				bean.setNome(rs.getString("Nome"));
+				bean.setDescr(rs.getString("Descrizione"));
+				bean.setQta(rs.getInt("Qta"));
+				bean.setDisp(rs.getBoolean("Disponibile"));
+				bean.setPicture((com.mysql.cj.jdbc.Blob) rs.getBlob("Foto"));
+				bean.setCosto(rs.getFloat("Costo"));
+				bean.setStelleTot(rs.getInt("stelleTot"));
+				bean.setRecTot(rs.getInt("recTot"));
+				bean.setProduttore(rs.getString("Produttore"));
+				bean.setGenere(rs.getString("Genere"));
+				bean.setPiattaforma(rs.getString("Piattaforma"));
+				bean.setTipoGioco(rs.getString("TipoGioco"));
+				bean.setTipoProdotto(rs.getString("TipoProdotto"));
+				bean.setCategoria(rs.getString("Categoria"));
+				bean.setEdizione(rs.getString("Edizione"));
+				
+				if(bean.isDisp() == false)
+					err = 1;
+				
+				if(err == 0)
+					prodotti.add(bean);
+			}
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+		return prodotti;
+	}
+}
+
