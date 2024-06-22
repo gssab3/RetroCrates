8cbfe1fa2263c0b751fba6c7062da46b04c69372
gssab3 <g.sabetta3@studenti.unisa.it>
2024-06-22 23:35:10 +0200

    ahia

--- db/RetroCratesDBsql.sql
+++ db/RetroCratesDBsql.sql
@@ -31,7 +31,7 @@ CREATE TABLE Prodotto(
 	Descrizione TEXT NOT NULL, 
 	Qta INT NOT NULL, -- >=0
 	Disponibile BOOLEAN, -- al posto di cancellarlo
-	Foto Blob,
+	Foto MediumBlob,
 	Costo DECIMAL(10,2) NOT NULL,
 	stelleTot INT NOT NULL,
 	
